d5e996d7b1fd05fbce4e179e6b01dd39f20f5a0d
suyeon <jso8787@naver.com>
2024-09-12 16:13:02 +0900

    add router

---
+++ src/routes/root.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../components/Sidebar"
+import Detail from "../components/Detail"
+
+export default function Root() {
+    return (
+        <>
+            <Sidebar />
+            <Detail />
+        </>
+    )
+}
